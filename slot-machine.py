53b72a3257eb6099241ae2bf6fc99eb89818459d
I2A4CCC <bissac280@gmail.com>
2024-11-08 12:35:50 -0500

    bday_modules

--- slot-machine.py
+++ slot-machine.py
@@ -4,11 +4,13 @@
 # importing module
 import random
 
+symbols = ['🍒',' 🍇', '🍉', '7️⃣']
+
 def play():
-  symbols = ['🍒',' 🍇', '🍉', '7️⃣']
   result = random.choices(symbols, k=3)
+
   print(result)
-  while result[0] != symbols[3] or result[1] != symbols[3] or result[2] != symbols[3]:
+  while result[0] != '7️⃣' or result[1] != '7️⃣' or result[2] != '7️⃣':
     symbols = ['🍒',' 🍇', '🍉', '7️⃣']
     result = random.choices(symbols, k=3)
     new_attempt = str(input('press enter to try again : '))
@@ -17,6 +19,8 @@ def play():
     else:
       print('Thanks for trying')
 
-  if result[0] == symbols[3] and result[1] == symbols[3] and result[2] == symbols[3]:
+  if result[0] == '7️⃣' and result[1] == '7️⃣' and result[2] == '7️⃣':
     print("Jackpot! 💰")
+    
+
 play()
